576937d3856a73acf9a2abc7eaa5bb8569ddcf22
chou0728 <dean34520@gmail.com>
2019-11-20 01:09:22 +0800

    broken homepage into smaller components and style menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DirectoryMenu from '../../components/directory/directory-menu.components';
+import './homepage.styles.scss';
+
+const functionName = () => (
+  <div className='homepage'>
+    <DirectoryMenu />
+  </div>
+);
+
+export default functionName;
