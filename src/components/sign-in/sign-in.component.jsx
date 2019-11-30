5a816419e7ee1379868e3faf89930b0ff596388d
chou0728 <dean34520@gmail.com>
2019-11-30 19:56:45 +0800

    Add reusable CustomButton component and FormInput component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './sign-in.styles.scss';
+import FormInput from '../../components/form-input/form-input.component';
+import CustomButton from '../../components/custom-button/custom-button.component';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      email: '',
+      password: ''
+    });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            required
+            type='email'
+            name='email'
+            label='email'
+            value={this.state.email}
+            handleChange={this.handleChange}
+          />
+          <FormInput
+            required
+            type='password'
+            name='password'
+            label='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+          />
+          <CustomButton type='submit'>Sign in</CustomButton>
+          <CustomButton onClick={signInWithGoogle}>
+            Sign in with Google
+          </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
