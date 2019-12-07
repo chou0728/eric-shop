b4b8b3d112ee983e3fe741f74c2372b28103254d
chou0728 <dean34520@gmail.com>
2019-12-07 18:03:01 +0800

    Added Collection page and set up selectCollection selector

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './collection.styles.scss';
+import { connect } from 'react-redux';
+import { selectCollection } from '../../redux/shop/shop.selectors';
+import CellectionItem from '../../components/collection-item/collection-item.component';
+
+const Collection = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CellectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(Collection);
