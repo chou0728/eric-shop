8e2ebef6531c5b41611bfb5b9865e7e077ebe21e
Eric Chou <eric.chou@hwaotech.com>
2020-08-31 15:08:49 +0800

    Updates

---
+++ precache-manifest.c644768ba82ad16ca7323b289ec7c256.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960a1c4f2132c35417bb73696d027499",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84a94204a57d35ff3e7d",
+    "url": "./static/css/main.78ece834.chunk.css"
+  },
+  {
+    "revision": "26528891716dedc93c29",
+    "url": "./static/js/2.d49754d5.chunk.js"
+  },
+  {
+    "revision": "84a94204a57d35ff3e7d",
+    "url": "./static/js/main.c0bb444f.chunk.js"
+  },
+  {
+    "revision": "05143002c5d4b76d39d4",
+    "url": "./static/js/runtime-main.491296c4.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "./static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "./static/media/shopping-bag.30f07071.svg"
+  }
+]);
